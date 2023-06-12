5326d03a15b63bc587413351978938384e838284
Codey V <codeyvanourkerk@avixi.com>
2023-06-12 13:02:19 -0600

    Changed file name; Updated file contents to TypeScript

--- DateUtils.ts
+++ DateUtils.ts
@@ -1,9 +1,17 @@
-function AppendDate(val, range) {
+/**
+ * Appends the current date to the supplied value
+ * @param val The value to modify
+ * @param range The range to m,odify
+ */
+function AppendDate(val: string, range: GoogleAppsScript.Spreadsheet.Range): void {
   val = `${val} (${new Date().toLocaleDateString()})`.replace('+', '');
   range.setValue(val);
 }
 
-function AppendDateToSelectedRange() {
+/**
+ * Appends the supplied date (using the UI) to the selected range
+ */
+function AppendDateToSelectedRange(): void {
   var ui = SpreadsheetApp.getUi();
   var response = ui.prompt('Input Date', 'Please input the desired date\n(Use no value for currrent date)', ui.ButtonSet.OK_CANCEL);
   if (response.getSelectedButton() == ui.Button.CANCEL) return;
@@ -11,7 +19,7 @@ function AppendDateToSelectedRange() {
   var activeSheet = SpreadsheetApp.getActiveSheet();
   var date = response.getResponseText() == "" ? new Date().toLocaleDateString() : response.getResponseText();
 
-  var selectedRange = activeSheet.getActiveRange();
+  var selectedRange = activeSheet.getActiveRange() as GoogleAppsScript.Spreadsheet.Range;
   var modifiedVals = selectedRange.getValues()
     .map(row =>
       row.map(cellVal => `${cellVal} (${date})`.replace('+', '')));
