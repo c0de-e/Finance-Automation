cc2b776c183523443197ead80ff28b16e4a7426c
Codey V <codeyvanourkerk@avixi.com>
2023-06-19 19:01:46 -0600

    Added live update preview of selected cells

--- Utils.ts
+++ Utils.ts
@@ -2,7 +2,7 @@ function MultiFormulaDialog() {
     let ui = SpreadsheetApp.getUi();
     const html = HtmlService.createHtmlOutputFromFile("multiFormula.html")
         .setWidth(300)
-        .setHeight(100)
+        .setHeight(150)
         .setSandboxMode(HtmlService.SandboxMode.IFRAME);
     ui.showModelessDialog(html, "Apply formula to selected cells");
 }
@@ -18,3 +18,15 @@ function MultiFormula(formula: string) {
     });
 }
 
+function EvaluateSelectionValues(formula: string): Object {
+    let sheet = SpreadsheetApp.getActiveSheet();
+    let ranges = sheet.getSelection().getActiveRangeList()?.getRanges();
+
+    let valsMap = new Object();
+    ranges?.forEach(range => {
+        let rangeVals = range.getValues();
+        rangeVals = rangeVals.map(row => row.map(cell => cell = `${formula.replace("#CELL", cell)}`));
+        valsMap[ range.getA1Notation() ] = rangeVals;
+    });
+    return valsMap;
+}
